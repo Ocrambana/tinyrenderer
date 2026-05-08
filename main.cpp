a4caff687349d1e9b9bbda7e64b0284f2dd8078c
ocrambana <coccato.m@gmail.com>
2026-05-08 17:59:15 +0200

    Added line method

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
-int main(int argc, char** argv) {
+void line(int ax, int ay, int bx, int by, TGAImage &img, const TGAColor &color);
+
+int main(int argc, char** argv) 
+{
     constexpr int width  = 64;
     constexpr int height = 64;
     TGAImage framebuffer(width, height, TGAImage::RGB);
@@ -16,6 +19,11 @@ int main(int argc, char** argv) {
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
 
+    line(ax,ay,bx,by,framebuffer, green);
+    line(cx,cy,bx,by,framebuffer, blue);
+    line(cx,cy,ax,ay,framebuffer, yellow);
+    line(ax,ay,cx,cy,framebuffer, red);
+    
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
     framebuffer.set(cx, cy, white);
@@ -24,3 +32,15 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+void line(int ax, int ay, int bx, int by, TGAImage &img, const TGAColor &color)
+{
+    float t{0};
+    int tx,ty;
+    while(t < 1.0f)
+    {
+        tx = static_cast<int>((1.0f - t) * ax + t * bx);
+        ty = static_cast<int>((1.0f - t) * ay + t * by);
+        img.set(tx,ty,color);
+        t += 0.01f;
+    }
+}

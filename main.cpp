080b4ebfdee18d939ff4018c0436cbc4c99a9266
ocrambana <coccato.m@gmail.com>
2026-06-08 17:52:45 +0200

    Test phong lighting

--- main.cpp
+++ main.cpp
@@ -23,6 +23,54 @@ struct RandomShader : IShader
 
     virtual std::pair<bool, TGAColor> fragment(const vec3 bar) const
     {
+
+        return {false, color};
+    }
+};
+
+struct PhongShader : IShader
+{
+    const Model &model;
+    vec3 tri[3];
+    vec3 sunPos;
+    double ambient;
+    double specularPower;
+    vec3 cameraPos;
+    
+    PhongShader(const Model &m) : model(m) {}
+    
+    virtual vec4 vertex(const int face, const int vert)
+    {
+        vec3 v = model.vert(face,vert);
+        vec4 gl_Position = ModelView * vec4{v.x,v.y,v.z,1.};
+        tri[vert] = gl_Position.xyz();
+        return Perspective * gl_Position;
+    }
+    
+    virtual std::pair<bool, TGAColor> fragment(const vec3 bar) const
+    {
+        vec3 ab = tri[1] - tri[0];
+        vec3 ac = tri[2] - tri[0];
+        vec3 n =  normalized(cross(ab,ac));
+        // vec3 center = (tri[0] + tri[1] + tri[2]) / 3.;
+
+        vec3 l = normalized(bar - sunPos);
+        double cos_alpha = n * l;
+        float diffuse = std::max(0., cos_alpha);
+
+        vec3 r = {
+            2. * cos_alpha * n.x - l.x,
+            2. * cos_alpha * n.y - l.y,
+            2. * cos_alpha * n.z - l.z
+        };
+        r = normalized(r);
+        vec3 v = normalized(cameraPos - bar);
+        double cos_beta = r * v;
+        double specular = std::pow(std::max(0., r.z), specularPower);
+        
+        double sum = ambient + .4 * diffuse + .9 * specular;
+        sum *= 255;
+        TGAColor color = {sum,sum,sum, 255};
         return {false, color};
     }
 };
@@ -51,10 +99,13 @@ int main(int argc, char** argv)
     Model m(argv[1]);
     for (int i = 0; i < m.nfaces(); i++)
     {
-        RandomShader shader(m);
+        PhongShader shader(m);
+        shader.sunPos = {2,0,0};
+        shader.ambient = .1;
+        shader.cameraPos = eye;
+        shader.specularPower = 32.0;
         for (int f = 0; f < m.nfaces(); f++)
         {
-            shader.color = {std::rand()%255,std::rand()%255,std::rand()%255,255};
             Triangle clip = {
                 shader.vertex(f,0),
                 shader.vertex(f,1),
